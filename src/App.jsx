ac5e4b6852e1831754bef1eabb3809245a0fbab0
Alto0327 <aj.silvaf2003@gmail.com>
2025-05-02 09:46:12 -0400

    Added Random DIce Value

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import Dice from "./Components/Dice"
 import './App.css'
+import { useState } from "react"
 
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice)
 
+  function generateAllNewDice(){
+    const newDice =[]    
+
+    for(let i =0; i < 10; i++){
+      const randomNum = Math.floor(Math.random() * 6) + 1 
+      newDice.push(randomNum)
+    }
+    return newDice
+  }
+
+  const diceElement = dice.map((num,i) => <Dice value={num} key={i}/>)
+  console.log(diceElement)
+  
   return (
     <main>
       <div className="dice-container">
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
-        <Dice value={1} />
+        {diceElement}
       </div>
     </main>
   )
