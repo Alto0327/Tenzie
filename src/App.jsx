ea78a1823bb3f14f272973a3a5fb9653196229c5
Alto0327 <aj.silvaf2003@gmail.com>
2025-05-05 10:09:43 -0400

    Accessibility Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import Dice from "./Components/Dice"
 import './App.css'
 import React from "react"
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { nanoid } from "nanoid"
 import ReactConfetti from "react-confetti"
 import { useWindowSize } from "@uidotdev/usehooks"
 
 function App() {
   const [dice, setDice] = useState(() => generateAllNewDice())
-  
+  const btnRef = useRef(null)
   const {width, height} = useWindowSize()
-  //Check if Game is won
+  
   const gameWon = dice.every(die => die.isHeld) &&
     dice.every(die => die.value === dice[0].value)
+
+  useEffect(()=>{
+    if(gameWon){
+      btnRef.current.focus()
+    }
+  },[gameWon])
+
 
   function generateAllNewDice(){
     const newDice =[]   
@@ -49,7 +56,7 @@ function App() {
       setDice(generateAllNewDice)
     }
   }
-  
+
   const diceElement = dice.map(dieobj => 
     <Dice 
       value={dieobj.value}  
@@ -61,12 +68,17 @@ function App() {
   return (
     <main>
       {gameWon && <ReactConfetti width={width} height={height}/> }
+      <div aria-live="polite" className="sr-only">
+        {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+      </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElement}
       </div> 
-      <button className="roll-btn" onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
+      <button ref={btnRef} className="roll-btn" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll"}
+      </button>
     </main>
   )
 }
